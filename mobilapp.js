6a4b2a24764d3985c79a6427272598f9914c4c82
Catiussia Nascimento <catiussia_tamara@hotmail.com>
2021-06-09 15:06:05 -0300

    Quanto deu, moço?

---
+++ mobilapp.js
@@ -0,0 +1,13 @@
+function solucao(tempo, distancia) {
+	//seu codigo aqui
+    if (tempo < 5){
+    console.log(600);
+    } else if (tempo >= 5 && tempo <=60 ){
+      console.log (tempo * 100 + distancia * 50);
+    } else if (tempo > 60 && distancia < 100 ){
+      console.log(200 * distancia);
+      }else if (distancia >=100){
+        console.log (150 * distancia);
+      }
+
+}
